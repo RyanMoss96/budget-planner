4610715a73db470033ffe4dbd050629192c49310
Ryan Moss <5788836+RyanMoss96@users.noreply.github.com>
2022-06-02 14:47:20 +0000

    Initial Commit

---
+++ api/budgetplanner/src/main/java/co/uk/ryanmoss/budgetplanner/BudgetplannerApplication.java
@@ -0,0 +1,13 @@
+package co.uk.ryanmoss.budgetplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetplannerApplication.class, args);
+	}
+
+}
---
+++ api/budgetplanner/src/test/java/co/uk/ryanmoss/budgetplanner/BudgetplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.uk.ryanmoss.budgetplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
